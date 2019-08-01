f3621417ccb78d2df83d757e9335444e92107ad3
kasthuridevan2021 <kasthuridevan.s@gmail.com>
2019-07-31 23:46:32 -0700

    the sub program

---
+++ src/Today/Test3.java
@@ -0,0 +1,25 @@
+package Today;
+
+public class Test3 {
+
+	
+		public void Sub()
+		{ 
+			
+		 int a = 10,b=20;
+		int sub=a-b;
+		 
+		 
+		System.out.println("the value is :"+"\t"+sub);
+		}
+		
+		
+		public static void main(String[] args)
+		{
+		
+		Test3 s=new Test3();
+		
+			s.Sub();
+
+}
+}
