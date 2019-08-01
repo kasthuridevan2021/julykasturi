23734b7e0cdb1237226823eb43179a88778167e1
kasthuridevan2021 <kasthuridevan.s@gmail.com>
2019-07-31 23:26:18 -0700

    new code added

---
+++ src/Today/Test2.java
@@ -0,0 +1,28 @@
+package Today;
+
+public class Test2{
+	
+	public void Sum()
+	{ 
+		
+	 int a = 10,b=20;
+	int sum=a+b;
+	 
+	 
+	System.out.println("the sum is :"+"\t"+sum);
+	}
+	
+	
+	public static void main(String[] args)
+	{
+	
+	Test2 s=new Test2();
+	
+		s.Sum();
+
+}
+
+}
+
+
+
